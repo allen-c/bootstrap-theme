935e95b1afdad714523acd7155f4f9834055f7fe
Allen C <allen.a.cole@gmail.com>
2015-03-12 14:56:15 -0400

    Add menu support, move closing content div tag
    Added WordPress Nav Menu support for 2 menus: a dropdown and a primary
    navigation header. Removed manually entered menu. Moved closing div for
    content to footer.php

--- footer.php
+++ footer.php
@@ -1,14 +1,15 @@
-<hr>
-
-      <footer class="text-center">
-        <p>Copyright info <?php echo date('Y'); ?></p>
-	<p><a href="#link">Link</a> - 
-	<a href="#link">Link</a></p>
-      </footer>
-
-    </div> <!-- /container -->
-
-    <?php wp_footer(); ?>
-
-  </body>
+</div><!-- Close content div -->
+<hr>
+
+      <footer class="text-center">
+        <p>Copyright info <?php echo date('Y'); ?></p>
+	<p><a href="#link">Link</a> - 
+	<a href="#link">Link</a></p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <?php wp_footer(); ?>
+
+  </body>
 </html>
--- functions.php
+++ functions.php
@@ -1,21 +1,31 @@
-<?php 
-
-function script_styles()
-{
-	/* Register JS */
-	wp_register_script('bootstrap_jquery',get_stylesheet_directory_uri().'/js/bootstrap.js');
-	
-	/* Register CSS */
-	wp_register_style('bootstrap_style',get_stylesheet_directory_uri().'/css/bootstrap.css');
-	wp_register_style('bootstrap-theme',get_stylesheet_directory_uri().'/css/bootstrap-theme.css');
-	
-
-	/* Enqueue */
-	wp_enqueue_script('bootstrap_jquery');
-	wp_enqueue_style('bootstrap_style');
-	wp_enqueue_style('bootstrap-theme');
-	
-}
-add_action('wp_enqueue_scripts','script_styles');
-
+<?php 
+
+function script_styles()
+{
+	/* Register JS */
+	wp_register_script('bootstrap_jquery',get_stylesheet_directory_uri().'/js/bootstrap.js');
+	
+	/* Register CSS */
+	wp_register_style('bootstrap_style',get_stylesheet_directory_uri().'/css/bootstrap.css');
+	wp_register_style('bootstrap-theme',get_stylesheet_directory_uri().'/css/bootstrap-theme.css');
+	
+
+	/* Enqueue */
+	wp_enqueue_script('bootstrap_jquery');
+	wp_enqueue_style('bootstrap_style');
+	wp_enqueue_style('bootstrap-theme');
+	
+}
+add_action('wp_enqueue_scripts','script_styles');
+
+function register_my_menus() {
+  register_nav_menus(
+    array(
+      'header-menu' => __( 'Header Menu' ),
+      'dropdown-menu' => __( 'Dropdown Menu' )
+    )
+  );
+}
+add_action( 'init', 'register_my_menus' );
+
 ?>
